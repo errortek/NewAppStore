a24d6569cc6ab7c44cc3a47ec3bf57d2ec7dd06e
jpb <jakub.baranski.33@gmail.com>
2024-08-21 21:01:31 +0100

    Add Search support

--- NewAppStore/Views/MainPage.xaml.cs
+++ NewAppStore/Views/MainPage.xaml.cs
@@ -1,7 +1,25 @@
-﻿namespace NewAppStore.Views;
+﻿using NewAppStore.Views.AppsPages;
+
+namespace NewAppStore.Views;
 
 public sealed partial class MainPage : Page
 {
+
+    private readonly List<string> Apps = new List<string>()
+    {
+        "UltraTextEdit UWP Plus",
+        "UTE UWP+",
+        "UltraTextEdit UWP+",
+        "Ivirius Text Editor",
+        "Ivirius Text Editor Free",
+        "IVR Text Editor Free",
+        "IVR Text Editor",
+        "IVR Free",
+        "Certificate Installer for MSIX",
+        "Certificate Installer",
+        "Cert Installer"
+    };
+
     public MainPage()
     {
         this.InitializeComponent();
@@ -41,5 +59,55 @@ public sealed partial class MainPage : Page
             element.RequestedTheme = ElementTheme.Light;
         }
     }
-}
 
+    private void AutoSuggestBox_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+    {
+        // Since selecting an item will also change the text,
+        // only listen to changes caused by user entering text.
+        if (args.Reason == AutoSuggestionBoxTextChangeReason.UserInput)
+        {
+            var suitableItems = new List<string>();
+            var splitText = sender.Text.ToLower().Split(" ");
+            foreach (var app in Apps)
+            {
+                var found = splitText.All((key) =>
+                {
+                    return app.ToLower().Contains(key);
+                });
+                if (found)
+                {
+                    suitableItems.Add(app);
+                }
+            }
+            if (suitableItems.Count == 0)
+            {
+                suitableItems.Add("No results found");
+            }
+            sender.ItemsSource = suitableItems;
+        }
+    }
+
+    private void AutoSuggestBox_SuggestionChosen(AutoSuggestBox sender, AutoSuggestBoxSuggestionChosenEventArgs args)
+    {
+        var chosen = args.SelectedItem as string;
+        if (chosen != null)
+        {
+            if (chosen == "Ivirius Text Editor" | chosen == "IVR Free" | chosen == "Ivirius Text Editor Free")
+            {
+                NavFrame.Navigate(typeof(IVRFree));
+            }
+            else if (chosen == "Certificate Installer for MSIX")
+            {
+                NavFrame.Navigate(typeof(CertificateInstallerForMSIX));
+            }
+            else if (chosen == "MyApp")
+            {
+            }
+            else if (chosen == "UltraTextEdit UWP Plus")
+            {
+                NavFrame.Navigate(typeof(UTEUWPPlus));
+            }
+        }
+    }
+
+}
